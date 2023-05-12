828510bf6e8281fcdc4002ea8569884bf83a677c
SafwanChowdhury <safwanrija@yahoo.co.uk>
2023-05-12 02:01:12 +0100

    updated db and population code to use real world data

--- populate_db.py
+++ populate_db.py
@@ -1,12 +1,13 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AirlineAPI.settings')
-from datetime import date
+from datetime import date, datetime, timedelta
 from faker import Faker
 from AirlineAPI.models import Location, Passenger, Reservation, Price, Plane, Flight
 import random
+from FlightRadar24.api import FlightRadar24API
 
-current_date = date.today()  # Get the current date
-
+current_date = date.today()  
+fr_api = FlightRadar24API()
 fake = Faker()
 
 Passenger.objects.all().delete()
@@ -16,14 +17,7 @@ Price.objects.all().delete()
 Plane.objects.all().delete()
 Location.objects.all().delete()
 
-# Define a list of cities and airport codes
-cities = [ ('New York', 'JFK'), ('Los Angeles', 'LAX'), ('London', 'LHR'), ('Paris', 'CDG'), ('Dubai', 'DXB'), ('Abu Dhabi', 'AUH'), ('Hong Kong', 'HKG'), ('Tokyo', 'HND'), ('Toronto', 'YYZ'), ('Dehli', 'DEL')]
-
-# Create locations
-locations = []
-for city, airport_id in cities:
-    location = Location.objects.create(airport_id=city)
-    locations.append(location)
+flights_info = fr_api.get_flights(airline = 'UAE')
 
 # Create passengers
 passengers = []
@@ -56,28 +50,41 @@ for i in range(10):
     )
     planes.append(plane)
 
-flights = []
-for i in range(50):
-    departure_location = random.choice(locations)
-    arrival_location = random.choice(locations)
-    while departure_location == arrival_location:
-        arrival_location = random.choice(locations)
-    fake_date = fake.future_date(end_date='+1y', tzinfo=None)  # Generate a random date after the current date
-    plane = random.choice(planes)
-    flight = Flight.objects.create(
-        flight_id=i + 1,
-        departure_airport=departure_location,
-        arrival_airport=arrival_location,
-        airline="Emirates",
-        passengers=0,
-        available_seats_economy=plane.num_economy_seats,
-        available_seats_business=plane.num_business_seats,
-        available_seats_first=plane.num_first_class_seats,
-        flight_duration=fake.time(),
-        dateOfDeparture=fake_date,
-        eta=fake.time(),
-        etd=fake.time(),
-        plane=plane,
-        price=random.choice(prices)
-    )
-    flights.append(flight)
+# Create flights with real-world data
+for i, flight_info in enumerate(flights_info):
+    try:
+        details = fr_api.get_flight_details(flight_info.id)
+        flight_info.set_flight_details(details)
+        
+        duration = int(flight_info.time_details['historical']['flighttime'])
+        arrival = fr_api.get_airport(flight_info.destination_airport_icao)
+        departure = fr_api.get_airport(flight_info.origin_airport_icao)
+        
+        departureCity, created = Location.objects.get_or_create(airport_id=departure['position']['region']['city'])
+        arrivalCity, created = Location.objects.get_or_create(airport_id=arrival['position']['region']['city'])
+        etd = str(fake.time())  
+        etd_datetime = datetime.strptime(etd, "%H:%M:%S")
+        flight_duration_timedelta = timedelta(seconds=duration)
+        duration = str(flight_duration_timedelta)
+        eta_datetime = etd_datetime + flight_duration_timedelta
+        eta = eta_datetime.strftime("%H:%M:%S")
+        fake_date = fake.future_date(end_date='+1y', tzinfo=None)  # Generate a random date after the current date
+        plane = random.choice(planes)
+        flight = Flight.objects.create(
+            flight_id=i + 1,
+            departure_airport=departureCity,
+            arrival_airport=arrivalCity,
+            airline="Emirates",
+            passengers=0,
+            available_seats_economy=plane.num_economy_seats,
+            available_seats_business=plane.num_business_seats,
+            available_seats_first=plane.num_first_class_seats,
+            flight_duration=duration,
+            dateOfDeparture=fake_date,
+            eta=eta,
+            etd=etd,
+            plane=plane,
+            price=random.choice(prices)
+        )
+    except Exception as e:
+        print(f"Failed to create flight: {e}")
